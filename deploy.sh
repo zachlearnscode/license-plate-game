c4ae8f69a0a7a1e2cb81cecdd6b230f4a88ad955
Zach <zmcnulty980@gmail.com>
2021-08-28 10:56:06 -0500

    modified deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,13 +13,13 @@ cd dist
 # echo 'www.example.com' > CNAME
 
 git init
-git add -A
+git add -a
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:zachlearnscode/license-plate-game.git main:gh-pages
+git push origin master -f git@github.com:zachlearnscode/license-plate-game.git main:gh-pages
 
 cd -
